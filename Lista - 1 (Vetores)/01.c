3e516b4c4d1d87071653a761a65b82cdb3285242
Lucas Vieira de Almeida <lucaseluan2000@gmail.com>
2021-08-30 19:11:31 -0300

    C++

---
+++ Lista - 1 (Vetores)/01.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#define TAM 3
+
+void preencher(float *V);
+float resp(float *V,float X);
+
+int main() {
+  float V[TAM],R=0,X;
+  preencher(V);
+  printf("Digite o valor para verificar:\n");
+  scanf("%f",&X);
+  R=resp(V,X);
+  if(R==1){
+    printf("O valor %f pertence ao vetor\n",X);
+  }else{
+    printf("O valor %f nao pertence ao vetor\n",X);
+  }
+  return 0;
+}
+
+void preencher(float *V){
+  int i;
+  printf("Digite os valores do vetor:\n");
+  for(i=0;i<TAM;i++){
+    scanf("%f",&V[i]);
+  }
+  printf("\n");
+}
+
+float resp(float *V,float X){
+  float R=0;
+  int i;
+  for(i=0;i<TAM;i++){
+    if(V[i]==X){
+      R=1;
+    }
+  }
+  return R;
+}
---
+++ Lista - 1 (Vetores)/02.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define TAM 15
+
+void preencher(int *V);
+void inverter(int *V);
+void imprimir(int *V);
+
+int main() {
+  int V[TAM];
+  preencher(V);
+  inverter(V);
+  imprimir(V);
+  return 0;
+}
+
+void preencher(int *V){
+  int i;
+  printf("Digite os valores do vetor:\n");
+  for(i=0;i<TAM;i++){
+    scanf("%d",&V[i]);
+  }
+  printf("\n");
+}
+
+void inverter(int *V){
+  int i,j=14,aux=0;
+  for(i=0;i<7;i++){
+    aux=V[i];
+    V[i]=V[j];
+    V[j]=aux;
+    j--;
+  }
+}
+
+void imprimir(int *V){
+  int i;
+  for(i=0;i<TAM;i++){
+    printf("%d\n",V[i]);
+  }
+}
